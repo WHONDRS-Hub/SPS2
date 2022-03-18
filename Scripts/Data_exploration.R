991f7aa1c8a1b2c7c8b0296c3b3bc3a7876b0b14
Vanessa Garayburu-Caruso <69696155+vgc40@users.noreply.github.com>
2022-03-18 15:29:49 -0700

    updated code

--- Scripts/Data_exploration.R
+++ Scripts/Data_exploration.R
@@ -6,9 +6,29 @@ library(ggthemes);library(ggpubr);library(dplyr); library(stringr);library(ggpmi
 
 #####################################################################################
 # Set directories
-home.dir = "//pnl/projects/SBR_SFA/RC2/VGC_Spatial_Study_2/Data/"
-output.dir = ("//pnl/projects/SBR_SFA/RC2/VGC_Spatial_Study/Output/")
+home.dir = "C:/Users/gara009/OneDrive - PNNL/Documents/GitHub/SPS2/Data/"
+output.dir = ("C:/Users/gara009/OneDrive - PNNL/Documents/GitHub/SPS2/Output/")
 setwd(output.dir) # setting working directory to the output directory
 
 geo.input = paste0(home.dir,"Geospatial_data/")
 shp.input = paste0(home.dir,"Shape_files_RC_model/")
+
+# Load data
+model.out = read.csv(paste0(geo.input,"RC2_model_inputs_outputs_all.csv"))
+stream.out = read.csv(paste0(geo.input,"RC2_all_stream_Attributes.csv"))
+topo = read.csv(paste0(geo.input,"RC2_all_topographic_Attributes.csv"))
+ppl = read.csv(paste0(geo.input,"RC2_all_Population_Regional_WaterUse_Attributes.csv"))
+land = read.csv(paste0(geo.input,"RC2_all_LandCover_Attributes.csv"))
+hydro = read.csv(paste0(geo.input,"RC2_all_Hydrologic_Attributes.csv"))
+climate = read.csv(paste0(geo.input,"RC2_all_Climateand WaterBalanceModel.csv"))
+loc = read.csv(paste0(geo.input,"022522_all_sites_NHD_streamline_matched_v2.csv"))
+
+# Merge all the dataframes by site_ID
+
+df = Reduce(function(x,y)merge(x,y, all = T, by = c('site_ID','COMID','ID')),list(topo,stream.out,climate,hydro,land,ppl)) #
+
+
+
+df.all = merge(df,unique(model.out), by = c("COMID","ID","StreamOrde"), all= T)
+
+
